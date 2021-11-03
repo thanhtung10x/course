7dd4cb347dd30e0b4e7143b5dd4d3e67ca507fad
Tran Thanh Tung <90493356+thanhtung10x@users.noreply.github.com>
2021-11-03 23:41:03 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const express = require('express');
 const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const route = require('./routes');
 const db = require('./config/db');
